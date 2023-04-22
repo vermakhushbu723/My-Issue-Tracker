cf703fa4c087ca2bc6fbb5472775e1c7a0804073
khushbu verma <vermakhushbu723@gmail.com>
2023-04-22 08:27:50 +0530

    first commit

--- config/mongoose.js
+++ config/mongoose.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose');
 
 // here we are using the MongoDB Url we defined in our ENV file
 console.log("thamitha",process.env.DB_URL);
-let DB_URL = "mongodb://0.0.0.0:27017/myapp";
+let DB_URL = "mongodb:// 0.0.0.0:27017/myapp";
 // let DB_URL="mongodb+srv://vermakhushbu723:4JlZW8Xc03X6UNR6@cluster0.g2sv4fr.mongodb.net/test"
 //connect to the database
 mongoose.connect(DB_URL)
